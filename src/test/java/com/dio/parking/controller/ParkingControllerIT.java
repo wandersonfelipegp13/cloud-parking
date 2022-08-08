ea021736190957fa7867631db1026a26589f102f
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-08-07 23:24:19 -0300

    Added Security

--- src/test/java/com/dio/parking/controller/ParkingControllerIT.java
+++ src/test/java/com/dio/parking/controller/ParkingControllerIT.java
@@ -26,6 +26,8 @@ class ParkingControllerIT extends AbstractContainerBase {
 	@Test
 	void whenFindAllThenCheckResult() {
 		RestAssured.given()
+				.auth()
+				.basic("user", "Dio@123456")
 				.when()
 				.get("/parking")
 				.then()
@@ -43,6 +45,8 @@ class ParkingControllerIT extends AbstractContainerBase {
 		
 		RestAssured.given()
 				.when()
+				.auth()
+				.basic("user", "Dio@123456")
 				.contentType(MediaType.APPLICATION_JSON_VALUE)
 				.body(createDTO)
 				.post("/parking")
